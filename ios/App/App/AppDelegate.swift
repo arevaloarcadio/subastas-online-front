11ad8b55d0ec3a3c7b5c16d87062950eda650162
user205772 <user205772@TX050.local>
2021-09-30 00:14:01 -0500

    git commit -m

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Capacitor
+import FBSDKCoreKit
+import GoogleSignIn
+import CapacitorCommunityAppleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,7 +11,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        FBSDKCoreKit.ApplicationDelegate.shared.application(
+           application,
+           didFinishLaunchingWithOptions: launchOptions
+       )
+
+       
+       return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -34,10 +43,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
-    }
+            // Called when the app was launched with a url. Feel free to add additional processing here,
+            // but if you want the App API to support tracking app url opens, make sure to keep this call
+            if (FBSDKCoreKit.ApplicationDelegate.shared.application(
+                app,
+                open: url,
+                sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+            )) {
+                return true;
+            } else {
+                return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+            }
+        }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         // Called when the app was launched with an activity, including Universal Links.
